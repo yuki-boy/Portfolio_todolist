4480eac77e64cf3da8acfd3dcc854ac70eee58e9
yuki-boy <issi0430bjc@gmail.com>
2021-05-25 16:43:02 +0900

    bootstrap導入

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -1,14 +1,19 @@
 <a href="{{ route('create_todo') }}"><button type="button" class="btn btn-primary">タスクの追加</button></a>
 
-<table border="1">
-  <tr>
-    <th>タスクの名前</th>
-    <th>タスクの説明</th>
-    <th>タスクの時間(h)</th>
-    <th colspan="2">操作</th>
-  </tr>
+<div class="py-12">
+  <div class="container">
 
+<table border="1">
+  <thead>
+    <tr>
+      <th>タスクの名前</th>
+      <th>タスクの説明</th>
+      <th>タスクの時間(h)</th>
+      <th colspan="2">操作</th>
+    </tr>
+  </thead>
   @foreach($todos as $todo)
+  <tbody>
     <tr>
       <td>{{ $todo->task_name }}</td>
       <td>{{ $todo->task_description }}</td>
@@ -16,6 +21,10 @@
       <td><a href="{{ route('edit_page', ['id' => $todo->id]) }}"><button type="button" class="btn btn-primary">タスクの編集</button></a></td>
       <td><a href="{{ route('delete_page', ['id' => $todo->id]) }}"><button type="button" class="btn btn-primary">タスクの削除</button></a></td>
     </tr>
+  </tbody>
   @endforeach
 
 </table>
+
+</div>
+  </div>
